072a0572acae87d0f6d197187198cd64757d54a6
Daryl Stronge <darylkstronge@gmail.com>
2025-01-08 21:01:31 -0800

    use lxml parser for get_team_and_opp_stats

--- basketball_reference_scraper/teams.py
+++ basketball_reference_scraper/teams.py
@@ -114,7 +114,7 @@ def get_team_and_opp_stats(team, season_end_year, data_format="TOTALS"):
 
     if r.status_code == 200:
         print(r.content)
-        soup = BeautifulSoup(r.content, "html.parser")
+        soup = BeautifulSoup(r.content, "lxml")
         table = soup.find("table", {"id": "team_and_opponent", "class": "sortable stats_table"})
         print(table)
     else:
