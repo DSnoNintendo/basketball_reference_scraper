9c2e401fe66c6f73bb750756374780c051dc5dcd
Daryl Stronge <darylkstronge@gmail.com>
2025-01-11 21:10:46 -0800

    init branch

--- basketball_reference_scraper/request_utils.py
+++ basketball_reference_scraper/request_utils.py
@@ -2,12 +2,12 @@ import os
 from os import getenv
 from time import sleep, time
 
-from proxy_cycling_webdriver.chrome import WebDriver
 from requests import get
-from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from seleniumwire import webdriver
+from proxy_cycler import ProxyCycler
 
 
 def build_proxy_list():
@@ -19,10 +19,9 @@ def build_proxy_list():
 
 options = Options()
 options.add_argument("--headless=new")
-driver = WebDriver(
-    options=options,
-    proxies=build_proxy_list(),  # TODO: allow no proxies
-)
+selenium_proxy_cycler = ProxyCycler(build_proxy_list())
+requests_proxy_cycler = ProxyCycler(build_proxy_list())
+driver = webdriver.Chrome(options=options)
 last_request = time()
 
 
@@ -34,7 +33,7 @@ def get_selenium_wrapper(url, xpath):
         try:
             sleep(3)
             print("current proxy", driver.current_proxy)
-            driver.get(url)
+            driver.get(url, seleniumwire_options={'proxy': selenium_proxy_cycler.cycle_proxies()})
             element = driver.find_element(By.XPATH, xpath)
             return f'<table>{element.get_attribute("innerHTML")}</table>'
         except Exception as e:
@@ -47,18 +46,11 @@ def get_selenium_wrapper(url, xpath):
 
 
 def get_wrapper(url):
-    global last_request
-    # Verify last request was 3 seconds ago
-    if 0 < time() - last_request < 3:
-        sleep(3)
-    last_request = time()
-    r = get(url)
     while True:
+        r = get(url, proxies=requests_proxy_cycler.cycle_proxies())
         if r.status_code == 200:
             return r
         elif r.status_code == 429:
-            retry_time = int(r.headers["Retry-After"])
-            print(f"Retrying after {retry_time} sec...")
-            sleep(retry_time)
+            print(f"Error code {r.status_code}. Cycling proxies")
         else:
             return r
